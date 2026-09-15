75f8b7ed656127239292fc9777f93a9f506a3547
DaviCouto-gd <davi.couto.nascimento@gmail.com>
2026-09-15 10:21:00 -0300

    (Featch) [PI-47] Conexão Banco

--- database.py
+++ database.py
@@ -1,1 +1,27 @@
 ##aqui vai ficar a conexão com o mysql
+
+import pymysql
+
+# Conexão com o banco
+conn = pymysql.connect(
+    host="localhost",
+    port=3306,
+    user="root",
+    password="nexus",
+    database="db_cartoes"
+)
+
+cursor = conn.cursor()
+
+# Insert básico
+sql = """
+
+"""
+valores = ()
+
+cursor.execute(sql, valores)
+conn.commit()  # necessário pra salvar o insert de verdade
+
+
+cursor.close()
+conn.close()
