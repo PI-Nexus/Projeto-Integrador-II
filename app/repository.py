943eaef8216abef4b9e7e48fbefa84d2bae2590c
Gabriel <gabriel.yfujimoto@gmail.com>
2026-09-16 17:35:09 -0300

    (SCRUM-6)[feat]: Adição de métodos para interação com banco

--- app/repository.py
+++ app/repository.py
@@ -1,1 +1,81 @@
 # Todas as consultas SQL (Lojas, Pré-qualificação, Cadastro)
+
+from database import get_connection
+
+
+#método para inserção na database
+#tab = nome da tabela, col = colunas da tabela, val = valores a serem inseridos
+#usa keywords insertdb(tab="tabNome"). Passe val e col como tuplas preferencialmente
+#ex:
+#insertdb(tab="pessoa", col=("nome", "idade"), val=("João", 20))
+#insertdb(tab="pessoa", val=("João", 20))
+def insertdb(
+        *, 
+        tab:str, 
+        val:tuple[any, ...], 
+        col:tuple[str, ...] = ()
+        ):
+        with get_connection() as conn:
+            with conn.cursor() as cur:
+                placeholders = ", ".join(["%s"] * len(val))
+                
+                query = f"""
+                    INSERT INTO {tab} ({", ".join(col)})
+                    VALUES ({placeholders})
+                """
+                cur.execute(query, val)
+
+
+#método para recuperar valores da database
+#col = colunas a serem selecionadas, filter = filtro do where
+#selectdb(tab="pessoa", col=("nome", "idade"), filter={"nome": "João"})
+#filter não é necessário
+
+def selectdb(
+        *, 
+        tab:str, 
+        col:tuple[str, ...], 
+        filter:dict[any] = {}
+        ) -> tuple:
+    with get_connection() as conn:
+        with conn.cursor() as cur:
+            if(len(filter) > 0):
+                col = ", ".join(col)
+                filter = ", ".join(f"{k} = '{v}'" for k, v in filter.items())
+    
+                query = f"""
+                select {col} from {tab}
+                where {filter}
+                """
+            else:
+                col = ", ".join(col)
+
+                query = f"""
+                select {col} from {tab}
+                """
+            
+            cur.execute(query)
+            result = cur.fetchall()
+            return result
+
+#método para alterar valor na database
+#col = colunas a serem alteradas, filter = filtro do where
+#updatedb(tab="pessoa", col={"idade": 15 }, filter={"nome": "João"})
+def updatedb(
+        *, 
+        tab:str, 
+        col:dict[any], 
+        filter:dict[str]
+        ):
+    with get_connection() as conn:
+        with conn.cursor() as cur:
+
+            col = ", ".join(f"{k} = {v}" for k, v in col.items())
+            filter = ", ".join(f"{k} = '{v}'" for k, v in filter.items())
+
+            query = f"""
+            UPDATE {tab} SET {col}
+            WHERE {filter}
+            """
+
+            cur.execute(query)
--- database.py
+++ database.py
@@ -1,27 +1,22 @@
 ##aqui vai ficar a conexão com o mysql
-
 import pymysql
+from contextlib import contextmanager
 
-# Conexão com o banco
-conn = pymysql.connect(
-    host="localhost",
-    port=3306,
-    user="root",
-    password="nexus",
-    database="db_cartoes"
-)
+@contextmanager
+def get_connection():
+    conn = pymysql.connect(
+        host="localhost",
+        port=3306,
+        user="root",
+        password="nexus",
+        database="db_cartoes"
+    )
 
-cursor = conn.cursor()
-
-# Insert básico
-sql = """
-
-"""
-valores = ()
-
-cursor.execute(sql, valores)
-conn.commit()  # necessário pra salvar o insert de verdade
-
-
-cursor.close()
-conn.close()
+    try:
+        yield conn
+        conn.commit()
+    except Exception:
+        conn.rollback()
+        raise
+    finally:
+        conn.close()
